3c13b4799397cfb0611c93634012fa4cbdacef37
k-garces <kellygarce@gmail.com>
2024-10-17 14:53:02 -0500

    agregados comentarios

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import { Course } from './course.js';
 
 import { dataCourses } from './dataCourses.js';
 
-// 1. cómo se referencian los objetos del DOM
+// 1. cómo se referencian los objetos del DOM desde el código TS
 let coursesTbody: HTMLElement = document.getElementById('courses')!;
 const btnfilterByName: HTMLElement = document.getElementById("button-filterByName")!;
 
@@ -20,7 +20,7 @@ totalCreditElm.innerHTML = `${getTotalCredits(dataCourses)}` //Cuando accedes a 
 // 2. cómo se crean nuevos elementos HTML
 function renderCoursesInTable(courses: Course[]): void {
   console.log('Desplegando cursos');
-  courses.forEach((course) => {
+  courses.forEach((course) => { // 4. explicar el arrow function
     let trElement = document.createElement("tr");
     trElement.innerHTML = `<td>${course.name}</td>
                            <td>${course.professor}</td>
@@ -30,7 +30,7 @@ function renderCoursesInTable(courses: Course[]): void {
 }
  
 
- 
+ // 4. Revisión por parte de los estudiantes
 
 function applyFilterByName() { 
   let text = inputSearchBox.value;
